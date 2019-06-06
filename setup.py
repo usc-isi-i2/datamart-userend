8c5b96c8ca3dec7f326b1876825fdbef3dc28e98
Ke-Thia Yao <kyao@isi.edu>
2019-06-05 18:55:31 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,4 @@
-from setuptools import setup
-from setuptools.command.install import install
-
-
-# class PostInstallCommand(install):
-#     def run(self):
-#         import subprocess
-#         command_add = "python -m spacy download en_core_web_sm"
-#         p = subprocess.Popen(command_add, stdout=subprocess.PIPE, shell=True, stderr=subprocess.STDOUT)
-#         while p.poll() == None:
-#             out = p.stdout.readline().strip()
-#             if out:
-#                 print (bytes.decode(out))
-#         install.run(self)
+from setuptools import setup, find_packages
 
 with open('requirements.txt', 'r') as f:
     install_requires = list()
@@ -21,28 +8,16 @@ with open('requirements.txt', 'r') as f:
         if re:
             install_requires.append(re)
 
-print(dependency_links)
-setup(name='Datamart-isi-userend',
-      version='2.0dev',
-      description='Data Augmentation',
-      author='ISI',
-      url='https://github.com/usc-isi-i2/datamart-userend',
-      packages=['datamart_isi',
-                'datamart_isi.joiners', 
-                'datamart_isi.joiners.join_feature', 
-                'datamart_isi.materializers',
-                'datamart_isi.materializers.parsers',
-                'datamart_isi.metadata',
-                'datamart_isi.profilers',
-                'datamart_isi.profilers.helpers',
-                'datamart_isi.utilities',
-               ],
-      # package_dir={'datamart': 'datamart'},
-      # package_data={'datamart': ['resources/*.json','resources/*.csv']},
+setup(name='datamart_isi',
+      version='2.0.dev1',
+      description='USC ISI implementation of D3M Datamart API',
+      author='USC ISI',
+      url='https://github.com/usc-isi-i2/datamart-userend/tree/d3m',
+      packages=find_packages(),
 
       install_requires=install_requires,
-      dependency_links=dependency_links,
-      # cmdclass={
-      #     'install': PostInstallCommand
-      # }
-      )
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ])
