7429bee4718546bbe18593e9a54a679f1b89912f
ckxz105 <liu494@usc.edu>
2019-07-19 16:06:17 -0700

    update config file

--- datamart_isi/config.py
+++ datamart_isi/config.py
@@ -3,3 +3,4 @@ wikidata_server = "http://kg2018a.isi.edu:8888/bigdata/namespace/wdq/sparql"
 general_search_server = "http://dsbox02.isi.edu:9001/blazegraph/namespace/datamart3/sparql"
 memcache_server = "dsbox02.isi.edu:11211"
 wikifier_server = "http://dsbox02.isi.edu/wikifier/get_identifiers"
+cache_expire_time = 3600*24
